6986ae30a4352f839071763768dd9ee1d9727dba
Anil Kumar <146966464+justanilkumar@users.noreply.github.com>
2026-09-10 10:19:04 +0530

    Time: 12 ms (85.54%) | Memory: 46.5 MB (63.46%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,23 +1,42 @@
+// class Solution {
+//     public String frequencySort(String s) {
+//       int n = s.length();
+//     Map<Character,Integer>map = new HashMap<>();
+//     for(char c:s.toCharArray()){
+//         map.put(c,map.getOrDefault(c,0)+1);
+//     }  
+//     List<Map.Entry<Character,Integer>>list = new ArrayList<>(map.entrySet());
+//     list.sort((a,b)->{
+//         int com = Integer.compare(b.getValue(),a.getValue());
+//         if(com!=0)return com;
+//         return Integer.compare(a.getValue(),b.getValue());
+//     });
+//     String ans="";
+//     for(Map.Entry<Character,Integer>entry:list){
+//         int f=entry.getValue();
+//         for(int i=0;i<f;i++){
+//             ans+=entry.getKey();
+//         }
+//     }
+//     return ans;
+//     }
+// }
+
 class Solution {
     public String frequencySort(String s) {
-      int n = s.length();
-    Map<Character,Integer>map = new HashMap<>();
+    HashMap<Character,Integer> map = new HashMap<>();
     for(char c:s.toCharArray()){
-        map.put(c,map.getOrDefault(c,0)+1);
-    }  
-    List<Map.Entry<Character,Integer>>list = new ArrayList<>(map.entrySet());
-    list.sort((a,b)->{
-        int com = Integer.compare(b.getValue(),a.getValue());
-        if(com!=0)return com;
-        return Integer.compare(a.getValue(),b.getValue());
-    });
-    String ans="";
-    for(Map.Entry<Character,Integer>entry:list){
-        int f=entry.getValue();
-        for(int i=0;i<f;i++){
-            ans+=entry.getKey();
-        }
+      map.put(c,map.getOrDefault(c,0)+1);
     }
-    return ans;
+  List<Map.Entry<Character,Integer>> list = new ArrayList<>(map.entrySet());
+  Collections.sort(list,(a,b)->Integer.compare(b.getValue(),a.getValue()));
+  StringBuilder sb = new StringBuilder();
+for(Map.Entry<Character,Integer> entry: list){
+    char ch = entry.getKey();
+    int freq = entry.getValue();
+    for(int i=0;i<freq;i++){
+        sb.append(ch);
     }
 }
+return sb.toString();
+} }
